daf24aa45ee2461a79bea5384a28dc78c07fc692
Fallou24 <falloundiaye0221@gmail.com>
2026-05-04 12:54:15 +0000

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
 import "./index.css";
 import "./i18n";
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/terangasejour",
+  base:"/",
   server: {
     host: "::",
     port: 8080,
